7e36d60f8ddf81084d23b9e427d1027dc4f8edbe
Prasoon Gupta <ishankgupta1may@gmail.com>
2026-04-23 18:41:34 +0530

    Package Name Updated

--- android/app/src/main/java/com/prasoonportfolioapp/MainActivity.kt
+++ android/app/src/main/java/com/prasoonportfolioapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myportfolioapp
+package com.prasoonportfolioapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "MyPortfolioApp"
+  override fun getMainComponentName(): String = "PrasoonPortfolioApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/prasoonportfolioapp/MainApplication.kt
+++ android/app/src/main/java/com/prasoonportfolioapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.myportfolioapp
+package com.prasoonportfolioapp
 
 import android.app.Application
 import com.facebook.react.PackageList
