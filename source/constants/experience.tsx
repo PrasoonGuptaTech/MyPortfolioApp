7bf84aab17ab8cabb4434429bed55fe9aa05f353
Prasoon Gupta <ishankgupta1may@gmail.com>
2026-06-01 17:45:27 +0530

    Add Experience Data

---
+++ source/constants/experience.tsx
@@ -0,0 +1,54 @@
+export const PortfolioExperience = [
+  {
+    id: 1,
+    companyName: 'Scans AI',
+    companyLogo: require('../assets/Images/ScansAICompanyLogo.png'),
+    jobTitle: 'React Native Developer',
+    jobImage: '',
+    isCuurentCompany: true,
+    jobStartingDate: 'Apr 2026',
+    jobEndingDate: '',
+    jobDescription:
+      'Developed and maintained a Ride-Hailing platform that assists automotive companies in hiring qualified drivers and verified vehicles through vehicle inspection and reporting workflows. Responsible for feature development, CI/CD pipeline management, application deployment, and release management across Google Play Store and Apple App Store.',
+    jobTechStack: ['React Native', 'CI/CD Pipelines', 'App Store', 'Google Play'],
+  },
+  {
+    id: 2,
+    companyName: 'Infosys Ltd.',
+    companyLogo: '',
+    jobTitle: 'Technology Lead',
+    jobImage: '',
+    isCuurentCompany: false,
+    jobStartingDate: 'July 2021',
+    jobEndingDate: 'Mar 2026',
+    jobDescription:
+      'Contributed to a consumer-facing E-Commerce mobile application, focusing on feature development and checkout workflows. Provided production support by collaborating with cross-functional teams to resolve critical issues efficiently. Played a key role in the Home Screen Revamp initiative, improving user experience, engagement, and application performance.',
+    jobTechStack: ['React Native', 'Typescript'],
+  },
+  {
+    id: 3,
+    companyName: 'Wizlite Innovation Center',
+    companyLogo: require('../assets/Images/WizliteCompanyLogo.png'),
+    jobTitle: 'React Native Developer',
+    jobImage: '',
+    isCuurentCompany: false,
+    jobStartingDate: 'Jan 2021',
+    jobEndingDate: 'Mar 2021',
+    jobDescription:
+      'Provided production and customer support for a Trade License Management Application used by the Mangalore Government. Responsible for troubleshooting issues, coordinating with cross-functional teams, and delivering timely resolutions to ensure uninterrupted license application and renewal services for shop owners.',
+    jobTechStack: ['React Native'],
+  },
+  {
+    id: 4,
+    companyName: 'RamaKrishna Electro Components Pvt Ltd',
+    companyLogo: require('../assets/Images/RamaKrishnaCompanyLogo.png'),
+    jobTitle: 'Field Application Engineer',
+    jobImage: '',
+    isCuurentCompany: false,
+    jobStartingDate: 'Nov 2019',
+    jobEndingDate: 'Nov 2020',
+    jobDescription:
+      'Provided technical support and consulting for Nuvoton and STMicroelectronics microcontrollers, helping clients with device selection, setup, integration, and troubleshooting. Collaborated with customers throughout the product development lifecycle to deliver efficient embedded solutions and support pre-sales activities.',
+    jobTechStack: [''],
+  },
+];
